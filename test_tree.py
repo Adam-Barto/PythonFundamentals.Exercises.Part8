b1ceca839e8c0cf0ef682112311580fe73ae9ffe
Adam-Barto <adam.jbarto@gmail.com>
2023-05-20 13:31:39 -0400

    Tree.py completed

---
+++ test_tree.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from unittest.mock import patch, mock_open
+from io import StringIO
+import file_reader
+
+
+class Test(TestCase):
+    def test_print_out(self):
+        self.fail()
+
+    def test_print_to_file(self):
+        self.fail()
+
+    def test_file_checker(self):
+        self.fail()
